00013788ac63e0caef1ede5127a0bcda374625bd
ITDranik <itdranik@gmail.com>
2021-05-09 20:14:04 +0300

    Add requests logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger.middleware';
 import { UrlGeneratorService } from './url-generator.service';
 
 @Module({
@@ -9,4 +10,8 @@ import { UrlGeneratorService } from './url-generator.service';
   controllers: [AppController],
   providers: [AppService, UrlGeneratorService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
