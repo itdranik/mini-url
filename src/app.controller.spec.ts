ba03fa24017e651a6d9f959cfd521a560be921a9
ITDranik <itdranik@gmail.com>
2021-05-09 14:05:30 +0300

    Add in-memory-shortener

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,64 @@
+import { HttpStatus, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UrlGeneratorService } from './url-generator.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
+  let urlGeneratorService: UrlGeneratorService;
 
   beforeEach(async () => {
+    process.env.DOMAIN = 'localhost:3000';
+    process.env.PORT = String(3000);
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, UrlGeneratorService]
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
+    urlGeneratorService = app.get<UrlGeneratorService>(UrlGeneratorService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getWelcomePage', () => {
+    it('should return a welcoming message', () => {
+      expect(appController.getWelcomePage()).toBe('Welcome to yet another URL shortener');
+    });
+  });
+
+  describe('create', () => {
+    it('should generate a new short URL', () => {
+      jest.spyOn(appService, 'createShortUrl').mockReturnValueOnce('abcd777');
+
+      expect(appController.create({ originalUrl: 'https://example.com' })).toBe(
+        'localhost:3000/abcd777'
+      );
+    });
+
+    it('should throw InternalServerErrorException when a short URL isa not generated', () => {
+      jest.spyOn(urlGeneratorService, 'generate').mockReturnValue('aaaaaaa');
+      appController.create({ originalUrl: 'https://first-example.com' });
+
+      expect(() =>
+        appController.create({ originalUrl: 'https://second-example.com' })
+      ).toThrowError(InternalServerErrorException);
+    });
+  });
+
+  describe('redirect', () => {
+    it('should return the original link', () => {
+      jest.spyOn(appService, 'getOriginal').mockReturnValueOnce('http://example.com');
+      expect(appController.redirect('abcdef1')).toEqual({
+        url: 'http://example.com',
+        statusCode: HttpStatus.FOUND
+      });
+    });
+
+    it('should throw NotFoundException if the short URL is unknown', () => {
+      expect(() => appController.redirect('abcdef1')).toThrowError(NotFoundException);
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { CreateShortUrlDto } from './create-short-url.dto';
+import { UrlGeneratorService } from './url-generator.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private static readonly GENERATE_ATTEMPTS_MAX_COUNT = 5;
+  private static readonly SHORT_URL_LENGTH = 7;
+
+  constructor(private readonly urlGeneratorService: UrlGeneratorService) {}
+
+  private readonly logger = new Logger(AppService.name);
+
+  private readonly mapping = new Map<string, string>();
+
+  getWelcomePage(): string {
+    return 'Welcome to yet another URL shortener';
+  }
+
+  getOriginal(shortUrl: string): string | null {
+    return this.mapping.get(shortUrl) ?? null;
+  }
+
+  createShortUrl(createShortUrlDto: CreateShortUrlDto): string {
+    for (let i = 0; i < AppService.GENERATE_ATTEMPTS_MAX_COUNT; ++i) {
+      const shortUrl = this.urlGeneratorService.generate(AppService.SHORT_URL_LENGTH);
+      if (!this.mapping.has(shortUrl)) {
+        this.mapping.set(shortUrl, createShortUrlDto.originalUrl);
+        return shortUrl;
+      }
+    }
+
+    this.logger.error(
+      [
+        `Short url wasn't generated after ${AppService.GENERATE_ATTEMPTS_MAX_COUNT} attempts`,
+        'due to collisions, consider increasing the length of short URLs'
+      ].join(' ')
+    );
+    throw new InternalServerErrorException('Oops... Something wrong happened');
   }
 }
